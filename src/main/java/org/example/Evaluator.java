10eec9412a5101e3a8d2351390eb3c47a9fccfcc
Grigorii Dukach <dukach055@yandex.ru>
2024-12-25 20:56:02 +0300

    Реализация метдоа displayResults для класса Evaluator

--- src/main/java/org/example/Evaluator.java
+++ src/main/java/org/example/Evaluator.java
@@ -19,4 +19,23 @@ public class Evaluator {
         this.linkEval = new LinkEval(givenCount);
     }
 
+    /**
+     * Метод для вывода сравнения времени выполнения операций.
+     */
+    public void displayResults() {
+        long addListTime = listEval.timeAdd();
+        long getListTime = listEval.timeGet();
+        long removeListTime = listEval.timeRemove();
+
+        long addLinkTime = linkEval.timeAdd();
+        long getLinkTime = linkEval.timeGet();
+        long removeLinkTime = linkEval.timeRemove();
+
+        System.out.println("==============================================================================");
+        System.out.println("| Collection | Number of items | ms adding | ms getting | ms removing       |");
+        System.out.println("==============================================================================");
+        System.out.printf("| %-10s | %-14d | %-10d | %-10d | %-10d |\n", "ArrayList", count, addListTime, getListTime, removeListTime);
+        System.out.printf("| %-10s | %-14d | %-10d | %-10d | %-10d |\n", "LinkedList", count, addLinkTime, getLinkTime, removeLinkTime);
+        System.out.println("==============================================================================");
+    }
 }
