c13bbd86b0e50f3fd92c303ce4ae1bc17356ee12
Grigorii Dukach <dukach055@yandex.ru>
2024-12-25 20:41:06 +0300

    Написание полей и конструктора для класса Evaluater

--- src/main/java/org/example/Evaluator.java
+++ src/main/java/org/example/Evaluator.java
@@ -1,4 +1,22 @@
 package org.example;
 
+/**
+ * Класс для сравнения производительности ArrayList и LinkedList.
+ */
 public class Evaluator {
+    private int count;
+    private Eval listEval;
+    private Eval linkEval;
+
+    /**
+     * Конструктор класса Evaluator.
+     *
+     * @param givenCount количество операций
+     */
+    public Evaluator(int givenCount) {
+        this.count = givenCount;
+        this.listEval = new ListEval(givenCount);
+        this.linkEval = new LinkEval(givenCount);
+    }
+
 }
