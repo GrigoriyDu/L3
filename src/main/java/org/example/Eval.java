3b11f263af3488983a28a376c262aa7aa75386d1
Grigorii Dukach <dukach055@yandex.ru>
2024-12-25 20:37:34 +0300

    Создание интерфейса Eval для измерения производительности операцйи с коллекциями

--- src/main/java/org/example/Eval.java
+++ src/main/java/org/example/Eval.java
@@ -1,4 +1,44 @@
 package org.example;
 
-public class Eval {
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * Интерфейс для измерения производительности операций с коллекциями.
+ */
+public interface Eval {
+
+    /**
+     * Метод для измерения времени добавления элементов.
+     *
+     * @return время выполнения в миллисекундах
+     */
+    long timeAdd();
+
+    /**
+     * Метод для измерения времени получения элементов.
+     *
+     * @return время выполнения в миллисекундах
+     */
+    long timeGet();
+
+    /**
+     * Метод для измерения времени удаления элементов.
+     *
+     * @return время выполнения в миллисекундах
+     */
+    long timeRemove();
+
+    /**
+     * Метод для измерения времени выполнения заданного кода.
+     *
+     * @param task задача для измерения времени
+     * @return время выполнения в миллисекундах
+     */
+    default long measureTime(Runnable task) {
+        Instant startTime = Instant.now();
+        task.run();
+        Instant endTime = Instant.now();
+        return Duration.between(startTime, endTime).toMillis();
+    }
 }
